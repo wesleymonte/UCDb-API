57ba13b879b72316800c1da7b88af39807d681b3
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-06-26 01:53:11 -0300

    Added JWTAuthorizationFilter

--- src/main/java/com/ufcg/psoft/ucdb/core/config/SecurityConfig.java
+++ src/main/java/com/ufcg/psoft/ucdb/core/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ufcg.psoft.ucdb.core.config;
 
 import com.ufcg.psoft.ucdb.core.security.JWTAuthenticationFilter;
+import com.ufcg.psoft.ucdb.core.security.JWTAuthorizationFilter;
 import com.ufcg.psoft.ucdb.core.security.JWTUtil;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private Environment env;
 
     private static final String[] PUBLIC_MATCHERS = {
-        "/h2/**",
-        "/v1/**"
+        "/h2/**"
     };
 
-    private static final String[] PUBLIC_MATCHERS_GET = {
+    private static final String[] PUBLIC_MATCHERS_POST = {
+        "/v1/user"
     };
 
     @Override
@@ -47,8 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
 //            .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
             .antMatchers(PUBLIC_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
             .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
