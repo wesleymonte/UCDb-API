ea69d247baaf898bb0762b745c1ed588c8584527
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-07-02 08:52:15 -0300

    Open version endpoint

--- src/main/java/com/ufcg/psoft/ucdb/core/config/SecurityConfig.java
+++ src/main/java/com/ufcg/psoft/ucdb/core/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS = {
         "/h2/**",
+        "/v1/version"
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {
